03b8dd43e2d3cb493d78858ea33d1ed46b2b1d72
SaarYogev <saaryogev@gmail.com>
2016-06-07 10:53:28 +0300

    Improving addition of new parts with Reflection
    **Highly experimental**, so I'm going to the workshop for testing soon.

--- SFX Dashboard Widgets/src/application/NetworkTablesReader.java
+++ SFX Dashboard Widgets/src/application/NetworkTablesReader.java
@@ -1,16 +1,9 @@
 package application;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.tables.ITable;
 import edu.wpi.first.wpilibj.tables.ITableListener;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.fxml.FXML;
 
 public class NetworkTablesReader extends Thread implements ITableListener {
-	@FXML
-	public static SimpleBooleanProperty bool;
 	public static NetworkTable table;
 	
 	public void run() {
@@ -18,7 +11,6 @@ public class NetworkTablesReader extends Thread implements ITableListener {
 		NetworkTable.setIPAddress("10.22.31.5");
 		table = NetworkTable.getTable("costumDashboard");
 		table.addTableListener(this);
-		bool = new SimpleBooleanProperty(false);
 		new MainController().start();
 	}
 	
